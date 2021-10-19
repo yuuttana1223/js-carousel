a92f0d616a2ef1bbe68d1881724d1e67f7c11113
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-10-19 15:45:20 +0900

    [Add] jsでアニメーション追加

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
 "use strict";
 {
+  const next = document.getElementById("next");
+  const prev = document.getElementById("prev");
+
+  const ul = document.querySelector("ul");
+  const slides = ul.children;
+  let currentIndex = 0;
+
+  const updateButtons = () => {
+    prev.classList.remove("hidden");
+    next.classList.remove("hidden");
+    if (currentIndex === 0) {
+      prev.classList.add("hidden");
+    }
+    if (currentIndex === slides.length - 1) {
+      next.classList.add("hidden");
+    }
+  };
+
+  updateButtons();
+
+  next.addEventListener("click", () => {
+    currentIndex++;
+    updateButtons();
+    const slideWidth = slides[0].getBoundingClientRect().width;
+    // -を付ける理由はもともと左端中心から始まっているのでそれを負にずらすと右の要素が左に来る
+    ul.style.transform = `translateX(${-(slideWidth * currentIndex)}px)`;
+  });
+
+  prev.addEventListener("click", () => {
+    currentIndex--;
+    updateButtons();
+    const slideWidth = slides[0].getBoundingClientRect().width;
+    ul.style.transform = `translateX(${-(slideWidth * currentIndex)}px)`;
+  });
 }
