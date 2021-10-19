9f55a4f0c13dbe8bcd78188d02062709fc854973
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-10-19 16:46:32 +0900

    [Fisnish] カルーセル完成

--- js/main.js
+++ js/main.js
@@ -4,7 +4,10 @@
   const prev = document.getElementById("prev");
 
   const ul = document.querySelector("ul");
-  const slides = ul.children;
+  const slides = [...ul.children];
+  // dotsのステート
+  const dots = [];
+
   let currentIndex = 0;
 
   const updateButtons = () => {
@@ -20,12 +23,35 @@
 
   const moveSlides = () => {
     updateButtons();
+    updateDots();
+    // slides[0].clientWidthよりも小数点があり正確
     const slideWidth = slides[0].getBoundingClientRect().width;
     // -を付ける理由はもともと左端中心から始まっているのでそれを負にずらすと右の要素が左に来る
     ul.style.transform = `translateX(${-(slideWidth * currentIndex)}px)`;
   };
 
-  updateButtons();
+  const setupDots = () => {
+    slides.forEach((slide, index) => {
+      const button = document.createElement("button");
+      button.addEventListener("click", () => {
+        currentIndex = index;
+        moveSlides();
+      });
+      dots.push(button);
+      document.querySelector("nav").appendChild(button);
+    });
+    dots[0].classList.add("current");
+  };
+
+  const updateDots = () => {
+    dots.forEach((dot) => {
+      dot.classList.remove("current");
+    });
+    dots[currentIndex].classList.add("current");
+  };
+
+  updateButtons(); /* 最初は左のボタンが見えない */
+  setupDots(); /* 最初は左のドットが濃い */
 
   next.addEventListener("click", () => {
     currentIndex++;
@@ -34,6 +60,10 @@
 
   prev.addEventListener("click", () => {
     currentIndex--;
+    moveSlides();
+  });
+
+  window.addEventListener("resize", () => {
     moveSlides();
   });
 }
